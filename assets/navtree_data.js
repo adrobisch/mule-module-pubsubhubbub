fb21ea66fff79e8e4ddfb93c017ec43d3ed344c0
MuleSoft <code-contrib@mulesoft.com>
2011-10-07 17:25:39 -0700

    Updating documentation

---
+++ assets/navtree_data.js
@@ -0,0 +1,20 @@
+var NAVTREE_DATA =
+[ [ "org.mule.module.pubsubhubbub", "org/mule/module/pubsubhubbub/package-summary.html", [ [ "Classes", null, [ [ "Constants", "org/mule/module/pubsubhubbub/Constants.html", null, "" ], [ "HubModule", "org/mule/module/pubsubhubbub/HubModule.html", null, "" ], [ "HubResponse", "org/mule/module/pubsubhubbub/HubResponse.html", null, "" ], [ "HubUtils", "org/mule/module/pubsubhubbub/HubUtils.html", null, "" ] ]
+, "" ], [ "Enums", null, [ [ "HubMode", "org/mule/module/pubsubhubbub/HubMode.html", null, "" ], [ "VerificationType", "org/mule/module/pubsubhubbub/VerificationType.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.module.pubsubhubbub.config", "org/mule/module/pubsubhubbub/config/package-summary.html", [ [ "Classes", null, [ [ "HandleRequestMessageProcessor", "org/mule/module/pubsubhubbub/config/HandleRequestMessageProcessor.html", null, "" ], [ "HubModuleCapabilitiesAdapter", "org/mule/module/pubsubhubbub/config/HubModuleCapabilitiesAdapter.html", null, "" ], [ "HubModuleLifecycleAdapter", "org/mule/module/pubsubhubbub/config/HubModuleLifecycleAdapter.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.module.pubsubhubbub.config.spring", "org/mule/module/pubsubhubbub/config/spring/package-summary.html", [ [ "Classes", null, [ [ "HandleRequestDefinitionParser", "org/mule/module/pubsubhubbub/config/spring/HandleRequestDefinitionParser.html", null, "" ], [ "HubModuleConfigDefinitionParser", "org/mule/module/pubsubhubbub/config/spring/HubModuleConfigDefinitionParser.html", null, "" ], [ "HubModuleNamespaceHandler", "org/mule/module/pubsubhubbub/config/spring/HubModuleNamespaceHandler.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.module.pubsubhubbub.data", "org/mule/module/pubsubhubbub/data/package-summary.html", [ [ "Classes", null, [ [ "DataStore", "org/mule/module/pubsubhubbub/data/DataStore.html", null, "" ], [ "TopicSubscription", "org/mule/module/pubsubhubbub/data/TopicSubscription.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.module.pubsubhubbub.handler", "org/mule/module/pubsubhubbub/handler/package-summary.html", [ [ "Classes", null, [ [ "AbstractHubActionHandler", "org/mule/module/pubsubhubbub/handler/AbstractHubActionHandler.html", null, "" ], [ "PublisherHandler", "org/mule/module/pubsubhubbub/handler/PublisherHandler.html", null, "" ], [ "PublisherHandler.ContentDistributionContext", "org/mule/module/pubsubhubbub/handler/PublisherHandler.ContentDistributionContext.html", null, "" ], [ "PublisherHandler.ContentFetchWork", "org/mule/module/pubsubhubbub/handler/PublisherHandler.ContentFetchWork.html", null, "" ], [ "PublisherHandler.DistributeAuthenticatedContentRetryCallback", "org/mule/module/pubsubhubbub/handler/PublisherHandler.DistributeAuthenticatedContentRetryCallback.html", null, "" ], [ "PublisherHandler.DistributeContentRetryCallback", "org/mule/module/pubsubhubbub/handler/PublisherHandler.DistributeContentRetryCallback.html", null, "" ], [ "SubscriptionHandler", "org/mule/module/pubsubhubbub/handler/SubscriptionHandler.html", null, "" ], [ "UnsubscriptionHandler", "org/mule/module/pubsubhubbub/handler/UnsubscriptionHandler.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.module.pubsubhubbub.request", "org/mule/module/pubsubhubbub/request/package-summary.html", [ [ "Classes", null, [ [ "AbstractVerifiableRequest", "org/mule/module/pubsubhubbub/request/AbstractVerifiableRequest.html", null, "" ], [ "SubscriptionRequest", "org/mule/module/pubsubhubbub/request/SubscriptionRequest.html", null, "" ], [ "UnsubscriptionRequest", "org/mule/module/pubsubhubbub/request/UnsubscriptionRequest.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.module.pubsubhubbub.rome", "org/mule/module/pubsubhubbub/rome/package-summary.html", [ [ "Classes", null, [ [ "PerRequestUserAgentHttpClientFeedFetcher", "org/mule/module/pubsubhubbub/rome/PerRequestUserAgentHttpClientFeedFetcher.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
